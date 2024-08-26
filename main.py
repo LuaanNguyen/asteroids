c5e1c7f476fca605e2c9301a5cd3d91fbe1d538b
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-26 16:31:14 -0700

    Added Window

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pygame 
 from constants import * 
 
+#Initialize Pygame
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Asteroids')
+
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    #game loop 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+    
     
 if __name__ == "__main__":
     main()
