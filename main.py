700715a374fc25333b748e5d4ad8d4a919b4b625
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-27 08:54:11 -0700

    Adjusted Code Format

--- main.py
+++ main.py
@@ -2,24 +2,15 @@ import pygame
 from constants import * 
 from player import *
 
-#Initialize Pygame
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Asteroids')
-
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    #Initialize Pygame
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Asteroids')
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     time_clock= pygame.time.Clock()
     dt = 0 #delta time 
-    
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-    
+     
     #game loop 
     while True:
         for event in pygame.event.get():
@@ -29,8 +20,12 @@ def main():
         screen.fill((0, 0, 0))
         player.draw(screen)    
         pygame.display.flip()
+        
+        #limit the framerate to 60FPS
         tick = time_clock.tick(60)
         dt = tick / 1000
+        
+        
         
 if __name__ == "__main__":
     main()
